bbc3751458893396369fc4898b6c1358dbd2d0f0
Ramin Vakili <ramin.vakili@firstsource.tech>
2020-06-14 13:11:06 +0430

    Only RealTime chart on App's main page

--- lib/main.dart
+++ lib/main.dart
@@ -18,12 +18,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar : AppBar(
-          title: Text('Platform View'),
-        ),
-        body: ChartsList(),
-      ),
+      home: CandleChartPage(),
     );
   }
 }
