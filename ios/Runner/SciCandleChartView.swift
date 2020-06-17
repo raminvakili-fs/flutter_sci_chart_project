c965c625cc93015e989294792ab1939c8d9e9135
Ramin Vakili <ramin.vakili@firstsource.tech>
2020-06-17 13:48:06 +0430

    getting API data in iOS side

--- ios/Runner/SciCandleChartView.swift
+++ ios/Runner/SciCandleChartView.swift
@@ -26,15 +26,41 @@ public class SciCandleChartView: NSObject, FlutterPlatformView, WKScriptMessageH
         
         channel.setMethodCallHandler({
             (call: FlutterMethodCall, result: FlutterResult) -> Void in
-            if (call.method == "changeChartType") {
-                // let url = call.arguments as! String
-                // Do something!
-                print("changeChartType in Candle Chart")
+            
+            switch call.method {
+            case "changeChartType":
                 self.stockChartView.changeSeriesType(call.arguments as! String)
+                break
+            case "loadHistoryCandles":
+                self.loadHistoryCandles(argMap: call.arguments as! [String: Any])
+                break
+            case "addOHLC":
+                self.addOHLC(argMap: call.arguments as! [String: Any])
+                break
+            default:
+                print("Not implemented")
             }
         })
     }
     
+    private func loadHistoryCandles(argMap: [String: Any]) -> Void {
+        let candlesList = argMap["candles"] as! [[String: Any]]
+        
+        let priceSeries = SCDPriceSeries()
+        
+        for candleMap in candlesList {
+            priceSeries.add(SCDPriceBar(date: Date.init(timeIntervalSince1970: TimeInterval(candleMap["epoch"] as! Int64) / 1000), open: candleMap["open"] as? NSNumber, high: candleMap["high"] as? NSNumber, low: candleMap["low"] as? NSNumber, close: candleMap["close"] as? NSNumber, volume: 0))
+        }
+        
+        print(priceSeries)
+        
+    }
+    
+    private func addOHLC(argMap: [String: Any]) -> Void {
+        let newOHLC = SCDPriceBar(date: Date.init(timeIntervalSince1970: TimeInterval(argMap["open_time"] as! Int64) / 1000), open: argMap["open"] as? NSNumber, high: argMap["high"] as? NSNumber, low: argMap["low"] as? NSNumber, close: argMap["close"] as? NSNumber, volume: 0)
+        
+        print(newOHLC)
+    }
     
     public func view() -> UIView {
         return stockChartView.chartLayout
