78105bd19cb1d1367e18161998fbb90fcc94bf29
Ramin Vakili <ramin.vakili@firstsource.tech>
2020-06-23 18:20:57 +0430

    Some refactor

--- lib/candle_chart_page.dart
+++ lib/candle_chart_page.dart
@@ -12,6 +12,10 @@ class _CandleChartPageState extends State<CandleChartPage> {
   BinaryAPI _api;
   int _numOfTicks = 0;
   String _tickStyle = 'candles';
+  int _granularity = 60;
+  int _historySize = 1000;
+
+  bool _connected = false;
 
   @override
   void initState() {
@@ -22,23 +26,29 @@ class _CandleChartPageState extends State<CandleChartPage> {
   void _initStream() async {
     _api = BinaryAPI();
     await _api.run();
+    setState(() {
+      _connected = true;
+    });
     _subscribeTick();
   }
 
-  void _switchGranularity(int granularity) async {
+  void _reloadChart() async {
     await _api.unsubscribeAll(_tickStyle, shouldForced: true);
-    _tickStyle = _tickStyle = granularity == 1 ? 'ticks' : 'candles';
-    _subscribeTick(granularity: granularity);
+    _tickStyle = _granularity == 1 ? 'ticks' : 'candles';
+    _subscribeTick();
   }
 
-  void _subscribeTick({int granularity = 60}) {
+  void _subscribeTick() {
     final Map<String, dynamic> request = {
       'ticks_history': 'R_50',
       'adjust_start_time': 1,
-      'granularity': granularity > 1 ? granularity : null,
-      'count': 200,
+      'granularity': _granularity > 1 ? _granularity : null,
+      'count': _historySize,
       'end': 'latest',
-      'start': 1,
+      'start': DateTime.now()
+              .subtract(const Duration(days: 20))
+              .millisecondsSinceEpoch ~/
+          1000,
       'style': _tickStyle,
       'subscribe': 1,
     };
@@ -79,26 +89,49 @@ class _CandleChartPageState extends State<CandleChartPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Number of ticks: $_numOfTicks'),
+        title: Text(
+          'Ticks: $_numOfTicks',
+          style: TextStyle(fontSize: 10),
+        ),
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.arrow_forward),
             onPressed: () => _controller.scrollToCurrentTick(),
           ),
           PopupMenuButton<int>(
-            child: Center(
-                child: Padding(
-              padding: const EdgeInsets.only(right: 4),
-              child: Text('Granularity'),
-            )),
+            child: Container(
+              alignment: Alignment.center,
+              padding: const EdgeInsets.all(12),
+              child: Icon(Icons.access_time),
+            ),
             onSelected: (choice) {
-              _switchGranularity(choice);
+              _granularity = choice;
+              _reloadChart();
             },
             itemBuilder: (BuildContext context) {
               return {1, 60, 120, 180, 300, 600, 900, 3600}
                   .map((int choice) => PopupMenuItem<int>(
                         value: choice,
-                        child: Text('$choice'),
+                        child: Text('Granularity ${choice}s'),
+                      ))
+                  .toList();
+            },
+          ),
+          PopupMenuButton<int>(
+            child: Container(
+              alignment: Alignment.center,
+              padding: const EdgeInsets.all(12),
+              child: Icon(Icons.history),
+            ),
+            onSelected: (choice) {
+              _historySize = choice;
+              _reloadChart();
+            },
+            itemBuilder: (BuildContext context) {
+              return {100, 200, 500, 1000, 2000, 5000}
+                  .map((int choice) => PopupMenuItem<int>(
+                        value: choice,
+                        child: Text('History $choice'),
                       ))
                   .toList();
             },
@@ -137,8 +170,20 @@ class _CandleChartPageState extends State<CandleChartPage> {
                 ),
               ),
             ),
+          ),
+          Center(
+            child: _connected ? SizedBox.shrink() : CircularProgressIndicator(),
           )
         ],
+      ),
+    );
+  }
+
+  Widget _buildOptionButton(String text) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.only(right: 4),
+        child: Text(text),
       ),
     );
   }
